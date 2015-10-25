4f2d49f1834b531c9cf486ce4b7eeacd14ac2edf
wahopedia <root@wahopedia.com>
2015-10-25 08:55:45 -0700

    Update wakeup.py

--- wakeup.py
+++ wakeup.py
@@ -2,24 +2,17 @@
 
 import RPi.GPIO as GPIO
 import time
+import os
+import math
 
-duration = 10.0 # 0.5 * 3600.0 # in hours times 3600.0 (float) seconds per hour
-steps = 3
-stay_on = 5 # 0.5 * 3600 # in hours, time to stay on after full light
-min_brightness = 0.1
-max_brightness = 100.0
+duration = 30 * 60.0 # in minutes times 60.0 (float) seconds per hour
+stay_on = 15 * 60 # in minutes, time to stay on after full light
 
-GPIO.setmode(GPIO.BOARD)
+os.system("gpio -g mode 18 pwm")
 
-GPIO.setup(5, GPIO.OUT)
-
-p = GPIO.PWM(5, 300) #pin, hertz
-
-p.start(min_brightness)
-for(i in range(steps)):
-  print (duration / steps)
-  p.ChangeDutyCycle(min_brightness + (((max_brightness - min_brightness) / steps) * i))
-  time.sleep(duration / steps)
+for i in range(1025):
+   print i
+   os.system("gpio -g pwm 18 %d" % i)
+   time.sleep(duration / 1024.0)
 time.sleep(stay_on)
-p.stop()
-GPIO.cleanup()
+os.system("gpio -g pwm 18 0")
